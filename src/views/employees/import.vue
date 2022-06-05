3577d5f9ecf02c67b77633ce144a8a92c6d7fc7b
youngguyz <2281300996@qq.com>
2022-06-05 17:56:23 +0800

    feat:Excel数据转换

--- src/views/employees/import.vue
+++ src/views/employees/import.vue
@@ -1,27 +1,60 @@
 <template>
   <div style="margin-top:70px">
-    <upload-excel :on-success="handleSuccess" />
+    <upload-excel :on-success="handleSuccess" :before-upload="beforeUpload" />
   </div>
 </template>
 
 <script>
 export default {
   methods: {
-    // beforeUpload(file) {
-    //   const isLt1M = file.size / 1024 / 1024 < 1
-    //   if (isLt1M) {
-    //     return true
-    //   }
-    //   this.$message({
-    //     message: 'Please do not upload files larger than 1m in size.',
-    //     type: 'warning'
-    //   })
-    //   return false
-    // },
+    beforeUpload(file) {
+      const isLt1M = file.size / 1024 / 1024 < 1
+      if (isLt1M) {
+        return true
+      }
+      this.$message({
+        message: 'Please do not upload files larger than 1m in size.',
+        type: 'warning'
+      })
+      return false
+    },
     handleSuccess({ results, header }) {
-      console.log(results, header)
-      this.tableData = results
-      this.tableHeader = header
+      console.log('这是中文的结果', results, header)
+      console.log('这是转英文的结果', this.transExcel(results))
+      // this.tableData = results
+      // this.tableHeader = header
+    },
+    transExcel(reslut) {
+    // 把一个对象数组中的每个对象的属性名，从中文改成英文
+    // 思路：对于原数组每个对象来说
+    //    （1） 找出所有的中文key
+    //     (2)  得到对应的英文key
+    //     (3)  拼接一个新对象： 英文key:值
+      const mapInfo = {
+        '入职日期': 'timeOfEntry',
+        '手机号': 'mobile',
+        '姓名': 'username',
+        '转正日期': 'correctionTime',
+        '工号': 'workNumber',
+        '部门': 'departmentName',
+        '聘用形式': 'formOfEmployment'
+      }
+      return reslut.map(zhObj => {
+        const enObj = {}
+        // 获取中文数组的所有键值
+        // 1. 拿到所有的中文 key
+        // Object.keys() 获取对象所有的属性名
+        // zhKeys: ['入职日期', '姓名', '工号', '手机号', '转正日期', '部门']
+        const zhKeys = Object.keys(zhObj)
+        // 2. 将中文 key 转为英文 key
+        zhKeys.forEach(zhKey => {
+          // 根据入职日期中文, 取出对应的英文 timeOfEntry
+          const enkey = mapInfo[zhKey]
+          // 3. 拼装成英文对象
+          enObj[enkey] = zhObj[zhKey]
+        })
+        return enObj
+      })
     }
   }
 }
